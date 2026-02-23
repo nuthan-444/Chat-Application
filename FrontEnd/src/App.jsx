c3aa71bf9f7e61d77f218149f08936f929cc165b
NuthaN-444 <nuthanprasadkg@gmail.com>
2026-02-23 06:39:02 +0530

    Frontend --> App.jsx changes

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,19 +1,34 @@
 import React, { useEffect } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Chat from './Components/Chat'
 import { ChatMessagesProvider } from './Contexts/ChatContext'
+import Login from './Components/Login'
+import Signup from './Components/Signup'
+import Home from './Components/Home'
+import CreateJoin from './Components/CreateJoin'
 
 
 const App = () => {
 
-   
+
 
   return (
     <div>
-      <ChatMessagesProvider>
+      <BrowserRouter>
 
-        <Chat />
+        <ChatMessagesProvider>
 
-      </ChatMessagesProvider>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path='/createjoin' element={ <CreateJoin /> } />
+            <Route path="/chat" element={<Chat />} />
+          </Routes>
+
+        </ChatMessagesProvider>
+
+      </BrowserRouter>
     </div>
   )
 }
